55c2671a9a590e99c197200136e1bafe9159cd7a
Harshal7887h <158986590+Harshal7887h@users.noreply.github.com>
2025-04-03 12:56:49 +0530

    Add files via upload

---
+++ AQIMoniter.ino
@@ -0,0 +1,49 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
+
+char auth[] = "lBV2IdFDqSKkA1flTUHcvR80cGqF-imG";  
+char ssid[] = "realme 9 5G";  
+char pass[] = "Harshal7887###";
+
+
+#define DHTPIN D4  
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+#define MQ135PIN A0  
+
+BlynkTimer timer;
+
+
+void sendSensorData() {
+    float air_quality = analogRead(MQ135PIN);  
+    float temperature = dht.readTemperature();  
+    float humidity = dht.readHumidity();  
+
+    
+    Blynk.virtualWrite(V0, air_quality);
+    Blynk.virtualWrite(V1, temperature);
+    Blynk.virtualWrite(V2, humidity);
+
+    
+    if (air_quality > 400) {  
+        Blynk.logEvent("air_quality_alert", "⚠️ High Pollution Detected! Take Precautions.");
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    Blynk.begin(auth, ssid, pass);
+    dht.begin();
+
+    
+    timer.setInterval(5000L, sendSensorData);
+}
+
+void loop() {
+    Blynk.run();
+    timer.run();
+}
