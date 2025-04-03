9435b3071a216ea6bffc05bcde5503f042f47797
Harshal7887h <158986590+Harshal7887h@users.noreply.github.com>
2025-04-03 12:59:36 +0530

    AQIMoniter.ino

--- AQIMoniter.ino
+++ AQIMoniter.ino
@@ -3,16 +3,13 @@
 #include <BlynkSimpleEsp8266.h>
 #include <DHT.h>
 
-
 char auth[] = "lBV2IdFDqSKkA1flTUHcvR80cGqF-imG";  
 char ssid[] = "realme 9 5G";  
 char pass[] = "Harshal7887###";
 
-
 #define DHTPIN D4  
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
-
 #define MQ135PIN A0  
 
 BlynkTimer timer;
@@ -22,13 +19,11 @@ void sendSensorData() {
     float air_quality = analogRead(MQ135PIN);  
     float temperature = dht.readTemperature();  
     float humidity = dht.readHumidity();  
-
     
     Blynk.virtualWrite(V0, air_quality);
     Blynk.virtualWrite(V1, temperature);
     Blynk.virtualWrite(V2, humidity);
 
-    
     if (air_quality > 400) {  
         Blynk.logEvent("air_quality_alert", "⚠️ High Pollution Detected! Take Precautions.");
     }
@@ -38,11 +33,8 @@ void setup() {
     Serial.begin(115200);
     Blynk.begin(auth, ssid, pass);
     dht.begin();
-
-    
     timer.setInterval(5000L, sendSensorData);
 }
-
 void loop() {
     Blynk.run();
     timer.run();
